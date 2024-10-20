76735745d497e1e5670376533caa4b7ae52044e5
ClaytonMurray92 <clamurr@okstate.edu>
2024-10-20 16:45:49 -0400

    Add files via upload

---
+++ Week 2 Project 4.py
@@ -0,0 +1,10 @@
+radius = float(input("Enter Radius of the sphere: "))
+diameter = radius * 2
+circumference = 2 * 3.14 * radius
+surfaceArea = 4 * 3.14 * radius ** 2
+volume = (4/3) * (3.14 * radius ** 3)
+print("Diameter of the sphere is: ", diameter)
+print("Circumference of the sphere is: ", circumference)
+print("Surface area of the sphere is: ", surfaceArea)
+print("volume of the sphere is: ",volume)
+        
